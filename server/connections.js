700e9dd2fb90c79a5f76c552638507984fe70326
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2018-02-28 16:37:53 -0500

    fix connection

--- server/connections.js
+++ server/connections.js
@@ -1,14 +1,6 @@
 import config from './config/db.json';
 import mysql from 'mysql';
 
-const mysqlConnection = mysql.createPool(config.mysql);
+const pool = mysql.createPool(config.mysql);
 
-// mysqlConnection.connect(function(err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log('connected as id ' + mysqlConnection.threadId);
-// });
-
-export { mysqlConnection };
+export { pool, mysql };
