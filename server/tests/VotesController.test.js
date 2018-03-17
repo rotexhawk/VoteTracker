b94ba14eaa92ce7365976865249fee8d6501dc8c
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2018-03-17 15:20:56 -0400

    allow one csv file for testing purposes

--- server/tests/VotesController.test.js
+++ server/tests/VotesController.test.js
@@ -1,17 +1,20 @@
 import VotesController from '../controllers/VotesController';
-import Votes from '../models/Votes';
 import moment from 'moment';
 
-const votesController = new VotesController(moment('2018-03-06', 'YYYY-MM-DD'), 'primary', false);
+const votesController = new VotesController(
+	moment('2018-03-06', 'YYYY-MM-DD'),
+	'primary',
+	false
+);
 
 test('VotesController Year to be 2018', () => {
-    expect(votesController.getYear()).toBe(2018);
+	expect(votesController.getYear()).toBe(2018);
 });
 
 test('VotesController Get Update Method to be false', () => {
-    expect(votesController.getUpdate()).toBe(false);
+	expect(votesController.getUpdate()).toBe(false);
 });
 
 test('VotesController Table to be prim2018', () => {
-    expect(votesController.getTable()).toBe('prim2018');
+	expect(votesController.getTable()).toBe('prim2018');
 });
